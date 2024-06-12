a6d2cf4c8de79da5ee8fed0f4b6bcba4418c983b
ValeryM <muntyanw@gmail.com>
2024-06-12 14:00:24 +0300

    v11

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
     loadTree();
 
-    $('#create-root').on('click', function() {
-        $.post('php/nodes.php', { action: 'create_root' }, function(response) {
+    $('#create-root').on('click', function () {
+        $.post('php/nodes.php', { action: 'create_root' }, function (response) {
             const root = JSON.parse(response);
             $('#tree-container').append(createNodeElement(root.id, 'root'));
         });
     });
 
-    $(document).on('click', '.add-node', function() {
+    $(document).on('click', '.add-node', function () {
         const parentId = $(this).closest('.node').data('id');
         addNode(parentId);
     });
 
-    $(document).on('click', '.delete-node', function() {
+    $(document).on('click', '.delete-node', function () {
         const nodeId = $(this).closest('.node').data('id');
         $('#popup-message').text('Are you sure you want to delete this node?');
         $('#confirmation-popup').modal('show').data('id', nodeId);
         startCountdown(20);
     });
 
-    $('#confirm-delete, #cancel-delete').on('click', function() {
+    $('#confirm-delete, #cancel-delete').on('click', function () {
         stopCountdown();
     });
 
-    $(document).on('click', '.edit-node', function() {
+    $(document).on('click', '.edit-node', function () {
         const nodeId = $(this).closest('.node').data('id');
         const node = $(`[data-id=${nodeId}]`);
         const label = node.find('.node-label');
@@ -34,14 +34,14 @@ $(document).ready(function() {
         editor.show().find('input').focus().val(label.text());
     });
 
-    $(document).on('click', '.save-edit', function() {
+    $(document).on('click', '.save-edit', function () {
         const nodeId = $(this).closest('.node').data('id');
         const editor = $(this).closest('.node-editor');
         const newName = editor.find('input').val();
         const label = editor.siblings('.node-label');
 
         if (newName !== '') {
-            $.post('php/nodes.php', { action: 'edit_node', id: nodeId, name: newName }, function() {
+            $.post('php/nodes.php', { action: 'edit_node', id: nodeId, name: newName }, function () {
                 label.text(newName);
             });
         }
@@ -50,7 +50,7 @@ $(document).ready(function() {
         label.show();
     });
 
-    $(document).on('click', '.cancel-edit', function() {
+    $(document).on('click', '.cancel-edit', function () {
         const editor = $(this).closest('.node-editor');
         const label = editor.siblings('.node-label');
 
@@ -58,7 +58,7 @@ $(document).ready(function() {
         label.show();
     });
 
-    $(document).on('click', '.expand-node', function() {
+    $(document).on('click', '.expand-node', function () {
         const node = $(this).closest('.node');
         const children = node.children('.children');
         const isExpanded = children.is(':visible');
@@ -69,22 +69,20 @@ $(document).ready(function() {
     });
 
     function addNode(parentId) {
-        $.post('php/nodes.php', { action: 'add_node', parent_id: parentId }, function(response) {
+        $.post('php/nodes.php', { action: 'add_node', parent_id: parentId }, function (response) {
             const newNode = JSON.parse(response);
             const parentNode = $(`[data-id=${parentId}]`);
             const childrenContainer = parentNode.children('.children');
             const nodeElement = createNodeElement(newNode.id, newNode.name, newNode.hasChildren, newNode.expanded);
             childrenContainer.append(nodeElement);
-            if (newNode.hasChildren) {
-                const expandButton = parentNode.find('.expand-node');
-                if (!expandButton.length) {
-                    parentNode.prepend('<button class="expand-node btn btn-secondary">►</button>');
-                }
+            const expandButton = parentNode.find('.expand-node');
+            if (!expandButton.length) {
+                parentNode.prepend('<button class="expand-node btn btn-secondary">►</button>');
             }
             updateExpandButtons();
         });
     }
-    
+
 
     function createNodeElement(id, name, hasChildren, expanded) {
         return `<div class="node" data-id="${id}">
@@ -105,9 +103,9 @@ $(document).ready(function() {
                     <div class="children"></div>
                 </div>`;
     }
-    
+
     function updateExpandButtons() {
-        $('.node').each(function() {
+        $('.node').each(function () {
             const children = $(this).children('.children');
             const expandButton = $(this).find('.expand-node');
             const hasChildren = children.children().length > 0;
@@ -119,16 +117,16 @@ $(document).ready(function() {
             }
         });
     }
-    
+
 
     function loadTree() {
-        $.post('php/nodes.php', { action: 'get_nodes' }, function(response) {
+        $.post('php/nodes.php', { action: 'get_nodes' }, function (response) {
             const nodes = JSON.parse(response);
             const nodesById = {};
             nodes.forEach(node => {
                 nodesById[node.id] = node;
             });
-    
+
             const buildTree = (parentId, container) => {
                 nodes.filter(node => node.parent_id == parentId).forEach(node => {
                     const nodeElement = createNodeElement(node.id, node.name, node.has_children, node.expanded);
@@ -139,15 +137,15 @@ $(document).ready(function() {
                 });
                 updateExpandButtons(); // Обновляем состояние кнопок развертывания после построения дерева
             };
-    
+
             buildTree(null, $('#tree-container'));
         });
     }
-    
+
     function startCountdown(seconds) {
         let timer = seconds;
         $('#popup-timer').text(`Time Left: ${timer}s`);
-        const interval = setInterval(function() {
+        const interval = setInterval(function () {
             timer--;
             $('#popup-timer').text(`Time Left: ${timer}s`);
             if (timer <= 0) {
